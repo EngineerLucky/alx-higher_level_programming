48cfe8b52fb8f46b11adb60cbfc5b4b4f51c8cd7
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-01-22 16:44:29 -0800

    12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
-""" printer should replaces an element of a list in a specific position"""
-
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return(my_list)
-
-    length = len(my_list)
-
-    if idx > length - 1:
-        return(my_list)
-
-    my_list[idx] = element
-
-    return(my_list)
-
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
