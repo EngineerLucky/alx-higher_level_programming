90d7ec12cb3f53e1b05cf209881019d6f8487e28
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-02-21 16:59:21 -0800

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""The function MyInt contains a class MyInt that inherits from int.
+"""
+
+
+class MyInt(int):
+    """Defines the MyInt class."""
+
+    def __eq__(self, other):
+        """Sets the == behaviour."""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """Sets the != behaviour."""
+        return int(self) == other
