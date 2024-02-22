be435615e79395ac3f19ff519f1f0d7433ca6506
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-02-21 16:17:32 -0800

    6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""The function defines a JSON file-writing function."""
+"""The function save_to_json_file contains
+
+a function that creates an Object from a “JSON file”.
+"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
-
+def load_from_json_file(filename):
+    """Creates an Object from a “JSON file”."""
+    with open(filename, 'r') as f:
+        return json.load(f)
