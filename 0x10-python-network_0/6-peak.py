27ddab790d51b2b943c76747011dda3f5580e56d
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-05-09 13:23:53 -0700

    0x10. Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""A function that finds a peak for list of integers."""
+""" The Algorithms for a list of integers."""
 
 
 def find_peak(list_of_integers):
+    """ The function finds a peak in a list of unsorted integers."""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+        return list_of_integers[0]i
