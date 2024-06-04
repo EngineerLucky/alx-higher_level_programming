a9fc35c04a10686f43f7dfd82f252ca9c7d1b378
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-06-04 11:05:50 -0700

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const urllink = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(urllink, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    for (const character of characters) {
+      request.get(character, (err, response, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
+    }
+  }
+});
