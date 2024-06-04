eeea0890c15d9d37540b62cf88c6fe622ecd9e9f
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-06-04 10:25:33 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-
+// star wars title
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request.get(urllink, (err, res, body) => {
+request.get(url, (err, res, body) => {
   if (err) console.log(err);
   else console.log(JSON.parse(body).title);
 });
